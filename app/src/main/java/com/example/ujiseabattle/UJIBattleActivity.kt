975622720b9b5a69d4f928379dd8041df466dd60
Guillermo <guillearnautm@gmail.com>
2021-04-27 16:37:27 +0200

    project structure

---
+++ app/src/main/java/com/example/ujiseabattle/UJIBattleActivity.kt
@@ -0,0 +1,16 @@
+package com.example.ujiseabattle
+
+import android.util.DisplayMetrics
+import es.uji.vj1229.framework.GameActivity
+import es.uji.vj1229.framework.IGameController
+
+class UJIBattleActivity : GameActivity() {
+    override fun buildGameController(): IGameController {
+        val displayMetrics = DisplayMetrics()
+        @Suppress("DEPRECATION")
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+        return UJISeaController(displayMetrics.widthPixels,
+            displayMetrics.heightPixels)
+    }
+}
