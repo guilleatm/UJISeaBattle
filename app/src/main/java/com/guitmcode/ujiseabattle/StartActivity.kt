3ec15b14a5922bf3fd1a6cd6846a4672d71a72b6
Guillermo <guillearnautm@gmail.com>
2021-05-11 18:24:43 +0200

    button working smoothly

--- app/src/main/java/com/guitmcode/ujiseabattle/StartActivity.kt
+++ app/src/main/java/com/guitmcode/ujiseabattle/StartActivity.kt
@@ -1,6 +1,7 @@
 package com.guitmcode.ujiseabattle
 
 import android.content.Intent
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,6 +10,7 @@ class StartActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_start)
+		requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 	}
 
 	fun onPlayClick(view: View) {
